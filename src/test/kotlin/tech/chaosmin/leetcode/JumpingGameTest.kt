1252b4f1d8294612e154045aa2090fc38b191070
Romani min <chaosmin@hotmail.com>
2020-04-17 11:35:24 +0800

    2020.04.17 每日打卡

---
+++ src/test/kotlin/tech/chaosmin/leetcode/JumpingGameTest.kt
@@ -0,0 +1,32 @@
+package tech.chaosmin.leetcode
+
+import org.junit.Assert
+import org.junit.jupiter.api.Test
+
+/**
+ * 示例 1:
+ * 输入: [2,3,1,1,4]
+ * 输出: true
+ * 解释: 我们可以先跳 1 步，从位置 0 到达 位置 1, 然后再从位置 1 跳 3 步到达最后一个位置。
+ *
+ * 示例 2:
+ * 输入: [3,2,1,0,4]
+ * 输出: false
+ * 解释: 无论怎样，你总会到达索引为 3 的位置。但该位置的最大跳跃长度是 0 ， 所以你永远不可能到达最后一个位置。
+ *
+ * @author romani min
+ * @since 2020/4/17
+ */
+internal class JumpingGameTest {
+
+    @Test
+    fun canJump() {
+        val testCase1 = intArrayOf(2, 3, 1, 1, 4)
+        val result1 = JumpingGame.canJump(testCase1)
+        Assert.assertEquals(true, result1)
+
+        val testCase2 = intArrayOf(3, 2, 1, 0, 4)
+        val result2 = JumpingGame.canJump(testCase2)
+        Assert.assertEquals(false, result2)
+    }
+}
