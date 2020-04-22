06cddc0c8be57ba41ccf444781812bed8de82bec
Romani min <chaosmin@hotmail.com>
2020-04-22 16:48:33 +0800

    2020.04.22 每日打卡

---
+++ src/main/kotlin/tech/chaosmin/leetcode/TreeNode.kt
@@ -0,0 +1,10 @@
+package tech.chaosmin.leetcode
+
+/**
+ * @author romani min
+ * @since 2020/4/22
+ */
+class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
