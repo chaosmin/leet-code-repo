5dafd669d211987fc4727fec4441782ddc4e031f
Romani min <chaosmin@hotmail.com>
2020-05-11 11:09:18 +0800

    2020.05.11 每日打卡 Pow(x,n) (快速幂+迭代)

---
+++ src/main/kotlin/tech/chaosmin/leetcode/T0050PowXN.kt
@@ -0,0 +1,37 @@
+package tech.chaosmin.leetcode
+
+/**
+ * 50. Pow(x, n) | Difficulty: medium
+ *
+ * Implement pow(x, n), which calculates x raised to the power n (x^n).
+ *
+ * ************************************
+ *
+ * 执行用时 :192 ms, 在所有 Kotlin 提交中击败了22.22%的用户
+ * 内存消耗 :32.2 MB, 在所有 Kotlin 提交中击败了100.00%的用户
+ *
+ * @author romani min
+ * @since 2020/5/11
+ */
+object T0050PowXN {
+    private fun quickMul(x: Double, n: Long): Double {
+        var ans = 1.0
+        var tx = x
+        var nt = n
+        while (nt > 0) {
+            if (nt % 2 == 1L) {
+                ans *= tx
+            }
+            tx *= tx
+            nt /= 2
+        }
+        return ans
+    }
+
+    fun myPow(x: Double, n: Int): Double {
+        return when {
+            n >= 0 -> quickMul(x, n.toLong())
+            else -> 1.0 / quickMul(x, -n.toLong())
+        }
+    }
+}
---
+++ src/test/kotlin/tech/chaosmin/leetcode/T0050PowXNTest.kt
@@ -0,0 +1,45 @@
+package tech.chaosmin.leetcode
+
+import org.junit.Assert
+import org.junit.jupiter.api.Test
+
+/**
+ * Example 1:
+ * Input: 2.00000, 10
+ * Output: 1024.00000
+ *
+ * Example 2:
+ * Input: 2.10000, 3
+ * Output: 9.26100
+ *
+ * Example 3:
+ * Input: 2.00000, -2
+ * Output: 0.25000
+ * Explanation: 2-2 = 1/22 = 1/4 = 0.25
+ *
+ * @author romani min
+ * @since 2020/5/11
+ */
+internal class T0050PowXNTest {
+
+    @Test
+    fun myPow() {
+        val result1 = String.format("%.5f", T0050PowXN.myPow(2.00000, 10))
+        Assert.assertEquals("1024.00000", result1)
+
+        val result2 = String.format("%.5f", T0050PowXN.myPow(2.10000, 3))
+        Assert.assertEquals("9.26100", result2)
+
+        val result3 = String.format("%.5f", T0050PowXN.myPow(2.00000, -2))
+        Assert.assertEquals("0.25000", result3)
+
+        val result4 = String.format("%.5f", T0050PowXN.myPow(2.00000, -2147483648))
+        Assert.assertEquals("0.00000", result4)
+
+        val result5 = String.format("%.5f", T0050PowXN.myPow(1.00000, -2147483648))
+        Assert.assertEquals("1.00000", result5)
+
+        val result6 = String.format("%.5f", T0050PowXN.myPow(-1.00000, -2147483648))
+        Assert.assertEquals("1.00000", result6)
+    }
+}
