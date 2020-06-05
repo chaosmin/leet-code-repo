3a0c97bfdce7ffe3ed666bfa0a272e249d00bc46
Romani min <chaosmin@hotmail.com>
2020-06-05 10:38:45 +0800

    2020.06.05 每日打卡 顺时针打印矩阵 (模拟)

---
+++ src/test/kotlin/tech/chaosmin/leetcode/T1621LCOFTest.kt
@@ -0,0 +1,21 @@
+package tech.chaosmin.leetcode
+
+import org.junit.Assert
+import org.junit.jupiter.api.Test
+
+/**
+ * @author romani min
+ * @since 2020/6/5
+ */
+internal class T1621LCOFTest {
+
+    @Test
+    fun spiralOrder() {
+        val result1 = T1621LCOF.spiralOrder(arrayOf(intArrayOf(1, 2, 3), intArrayOf(4, 5, 6), intArrayOf(7, 8, 9)))
+        Assert.assertArrayEquals(intArrayOf(1, 2, 3, 6, 9, 8, 7, 4, 5), result1)
+
+        val result2 =
+            T1621LCOF.spiralOrder(arrayOf(intArrayOf(1, 2, 3, 4), intArrayOf(5, 6, 7, 8), intArrayOf(9, 10, 11, 12)))
+        Assert.assertArrayEquals(intArrayOf(1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7), result2)
+    }
+}
